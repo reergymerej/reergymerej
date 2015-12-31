d19036a71dc299bedb24ba88d7c12eb43a489a35
Jeremy Greer <jeremy.greer@prometheanworld.com>
2015-12-31 00:24:29 -0500

    add some rendering so we can actually see React do its thing

--- jsx/Content.jsx
+++ jsx/Content.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
 
 export default class Header extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            count: 0,
+            clicks: 0,
+        };
+    }
+
     render() {
+        const {count, clicks} = this.state;
+
         return (
-            <div>Hello.</div>
+            <div>
+                <p>Hello. { count }</p>
+                <button onClick={ this.onButtonClick.bind(this) }>click me</button>
+                <div>
+                    <small>click count: {clicks}</small>
+                </div>
+            </div>
         );
+    }
+
+    componentDidMount() {
+        setInterval(this.bumpCount.bind(this), 300);
+    }
+
+    bumpCount() {
+        this.setState({
+            count: this.state.count + 1,
+        });
+    }
+
+    onButtonClick() {
+        this.setState({
+            clicks: this.state.clicks + 1,
+        });
     }
 }
