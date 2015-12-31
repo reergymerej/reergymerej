95060c7d5d9f7608fd7d2ae212588a444a59fe54
Jeremy Greer <jeremy.greer@prometheanworld.com>
2015-12-30 23:24:10 -0500

    simplify build command, use rc files

--- jsx/main.jsx
+++ jsx/main.jsx
@@ -1,5 +1,9 @@
 const React = require('react');
+// import React from 'react';
 const ReactDOM = require('react-dom');
+
+console.log('foo');
+console.log('foo');
 
 ReactDOM.render(
   <h1>reerGymereJ</h1>,
