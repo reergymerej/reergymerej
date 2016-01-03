311498167d8fc06a7995a48d37b93351b9d2d61c
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-01-03 16:21:09 -0500

    restore original layout, skip router for now

--- jsx/main.jsx
+++ jsx/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom'
 import Layout from './Layout.jsx'
 import { Router, Route, Link, browserHistory } from 'react-router'
 
-// ReactDOM.render(
-//     <Layout />,
-//     document.getElementById('content')
-// );
+ReactDOM.render(
+    <Layout />,
+    document.getElementById('content')
+);
 
-ReactDOM.render((
-  <div>hello</div>
-), document.body);
+// ReactDOM.render((
+//   <div>hello</div>
+// ), document.body);
