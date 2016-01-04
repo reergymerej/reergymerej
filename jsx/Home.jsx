30d001397d3bafac1894cbd80f15924ece089b70
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-01-03 23:05:58 -0500

    add some silly content to home, add green border color

--- jsx/Home.jsx
+++ jsx/Home.jsx
@@ -11,11 +11,16 @@ export default class Header extends React.Component {
         return (
             <div className="page">
                 <div>
-                    <p>Hello.</p>
+                    <h2>Hello.</h2>
                     <p>I've decided to rewrite <a href="http://jeremygreer.herokuapp.com/#/">my site</a> using some new tools.</p>
                     <ul>
-                        <li>auth</li>
-                        <li>simple blog</li>
+                        <li>React</li>
+                        <li>react-router</li>
+                        <li>Babel</li>
+                        <li>Watchify</li>
+                        <li>less</li>
+                        <li>JSX</li>
+                        <li>eslint</li>
                     </ul>
                 </div>
             </div>
--- jsx/Navigation.jsx
+++ jsx/Navigation.jsx
@@ -5,6 +5,7 @@ export default class Navigation extends React.Component {
     render() {
         return (
             <ul className="navigation">
+              <li><Link to="/">Home</Link></li>
               <li><Link to="/foo">Foo</Link></li>
               <li><Link to="/bar">Bar</Link></li>
             </ul>
