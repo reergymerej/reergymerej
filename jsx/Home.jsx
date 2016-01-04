b56ec5ca3e8463163d2ef7cffc0b89f762d37eac
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-01-03 21:36:25 -0500

    add eslint rule for unused vars

--- jsx/Home.jsx
+++ jsx/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Header from './Header.jsx';
 
 export default class Home extends React.Component {
   render() {
--- jsx/main.jsx
+++ jsx/main.jsx
@@ -1,21 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Layout from './Layout.jsx'
-import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router'
-
-import Reergymerej from './Reergymerej.jsx'
-import Home from './Home.jsx'
-import Foo from './Foo.jsx'
-import Bar from './Bar.jsx'
+import Router from './Router.jsx'
 
 const content = document.getElementById('content');
 
-ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/" component={Reergymerej}>
-      <IndexRoute component={Home} />
-      <Route path="foo" component={Foo} />
-      <Route path="bar" component={Bar} />
-    </Route>
-  </Router>
-), content);
+ReactDOM.render(<Router />, content);
