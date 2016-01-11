3870799be8bd4d8a812ab812eb59d8b7a1564423
Jeremy Greer <jeremy.greer@prometheanworld.com>
2016-01-10 21:24:16 -0500

    abstract jquery ajax call

--- jsx/services/auth.jsx
+++ jsx/services/auth.jsx
@@ -1,30 +1,44 @@
 // TODO: define path for jQuery
 import $ from '../../bower_components/jquery/dist/jquery.min'
 
+const ajax = (options = {}) => {
+    return new Promise((resolve, reject) => {
+        options = Object.assign({
+            type: 'POST',
+            url: '/',
+            data: null,
+            success: resp => {
+                resolve(resp);
+            },
+            error: (jqXHR, textStatus, errorThrown) => {
+                reject(errorThrown);
+            }
+        }, options);
+
+        $.ajax(options);
+    });
+};
+
 export default {
     login: (username, password) => {
-        $.ajax({
-            type: 'POST',
+        ajax({
             url: '/login',
             data: {
                 username,
                 password
             }
-        });
+        }).then(resp => {
+            console.log(resp);
+        })
     },
 
     testAuth: () => {
-        return new Promise(resolve => {
-            $.ajax({
-                type: 'POST',
-                url: '/secured',
-                success: () => {
-                    resolve(true);
-                },
-                error: () => {
-                    resolve(false);
-                },
-            });
-        });
+        return ajax({
+            url: '/secured',
+        }).then(() => true, () => false);
+    },
+
+    getUser: () => {
+
     }
 }
