b24bdc6e16a55328287efd2abc2f07c514e89829
Jeremy Greer <jeremy.greer@prometheanworld.com>
2016-01-22 23:42:16 -0500

    get a couple circles bouncing around

---
+++ jsx/dirty-code/circle.jsx
@@ -0,0 +1,70 @@
+let circle = (options) => {
+    let {
+        canvas,
+        x, y,
+        radius,
+        fillStyle,
+        xSpeed = 0,
+        ySpeed = 0
+    } = options;
+
+    let _x = x;
+    let _y = y;
+    let _radius = radius;
+    let _xSpeed = xSpeed;
+    let _ySpeed = ySpeed;
+    let context = canvas.getContext('2d');
+    let {width: canvasWidth, height: canvasHeight} = canvas;
+
+    let me = {
+        draw: () => {
+            context.beginPath();
+            context.arc(_x, _y, _radius, 0, 2 * Math.PI, false);
+            context.fillStyle = fillStyle;
+            context.fill();
+            context.lineWidth = 5;
+            context.strokeStyle = '#000';
+            context.stroke();
+        },
+
+        move: (x, y) => {
+            _x += x;
+            _y += y;
+        },
+
+        setSpeed: (x, y) => {
+            // TODO: use vecotrs instead
+            _xSpeed = isNaN(x) ? _xSpeed : x;
+            _ySpeed = isNaN(y) ? _ySpeed : y;
+        },
+
+        updatePosition: () => {
+            me.move(_xSpeed, _ySpeed);
+            me.handleBounce();
+        },
+
+        handleBounce: () => {
+            if (_x > canvasWidth) {
+                let pastX = _x - canvasWidth;
+                _x -= pastX;
+                _xSpeed *= -1;
+            } else if (_x < 0) {
+                _x = -_x;
+                _xSpeed *= -1;
+            }
+
+            if (_y > canvasHeight) {
+                let pastY = _y - canvasHeight;
+                _y -= pastY;
+                _ySpeed *= -1;
+            } else if (_y < 0) {
+                _y = -_y;
+                _ySpeed *= -1;
+            }
+        },
+    };
+
+    return me;
+};
+
+export default circle;
--- jsx/dirty-code/dirtyCode.jsx
+++ jsx/dirty-code/dirtyCode.jsx
@@ -1,59 +1,7 @@
+import circle from './circle.jsx'
+
 let rand  = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-let circle = (canvas, x, y, radius, fillStyle) => {
-    let _x = x;
-    let _y = y;
-    let _radius = radius;
-    let _xSpeed = 0;
-    let _ySpeed = 0;
-    let context = canvas.getContext('2d');
-    let {width: canvasWidth, height: canvasHeight} = canvas;
-
-    let me = {
-        draw: () => {
-            context.beginPath();
-            context.arc(_x, _y, _radius, 0, 2 * Math.PI, false);
-            context.fillStyle = fillStyle;
-            context.fill();
-            context.lineWidth = 5;
-            context.strokeStyle = '#000';
-            context.stroke();
-        },
-
-        move: (x, y) => {
-            _x += x;
-            _y += y;
-        },
-
-        setSpeed: (x, y) => {
-            // TODO: use vecotrs instead
-            _xSpeed = isNaN(x) ? _xSpeed : x;
-            _ySpeed = isNaN(y) ? _ySpeed : y;
-        },
-
-        updatePosition: () => {
-            me.move(_xSpeed, _ySpeed);
-            me.handleBounce();
-        },
-
-        handleBounce: () => {
-            if (_x > canvasWidth) {
-                let pastX = _x - canvasWidth;
-                _x -= pastX;
-                _xSpeed *= -1;
-            }
-
-            if (_y > canvasHeight) {
-                let pastY = _y - canvasHeight;
-                _y -= pastY;
-                _ySpeed *= -1;
-            }
-        },
-    };
-
-    return me;
 };
 
 export default (canvas) => {
@@ -63,8 +11,28 @@ export default (canvas) => {
     const centerX = width / 2;
     const centerY = height / 2;
 
-    let hero = circle(canvas, centerX, centerY, 25, '#fff');
-    hero.setSpeed(rand(1, 3), rand(1, 3));
+    let hero = circle({
+        canvas,
+        x: centerX,
+        y: centerY,
+        radius: 25,
+        fillStyle: '#fff',
+        xSpeed: rand(2, 3),
+        ySpeed: rand(2, 3),
+    });
+
+    let devs = [
+        hero,
+        // circle({
+        //     canvas,
+        //     x: centerX,
+        //     y: centerY,
+        //     radius: 20,
+        //     fillStyle: '#000',
+        //     xSpeed: rand(2, 3),
+        //     ySpeed: rand(2, 3),
+        // })
+    ];
 
     let clearCanvas = () => {
         context.fillStyle = '#fff';
@@ -73,14 +41,18 @@ export default (canvas) => {
 
     let loop = () => {
         // update actors
-        hero.updatePosition();
+        devs.forEach(dev => {
+            dev.updatePosition();
+        });
 
         clearCanvas();
 
         // save canvas state
 
         // draw
-        hero.draw();
+        devs.forEach(dev => {
+            dev.draw();
+        });
 
         // restore canvas state
 
